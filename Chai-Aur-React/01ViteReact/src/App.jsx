37ca3c493736a3da63a4316827b7712d3a69dbbc
Abhiraj Yadav <abhiraj100right@gmail.com>
2024-07-22 01:25:07 +0530

    Understand the react flow and structure

--- Chai-Aur-React/01ViteReact/src/App.jsx
+++ Chai-Aur-React/01ViteReact/src/App.jsx
@@ -1,11 +1,16 @@
+import Chai from "./Chai.jsx";
 
 function App() {
 
   return (
     <>
       <h1>Hello World with React Vite</h1>
+      <Chai />
+      <h2>chai le lo</h2>
     </>
-  )
+  );
 }
 
 export default App;
+
+//  <> </>   => react fragment
